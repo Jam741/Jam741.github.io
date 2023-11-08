d33b3beebae79dbfb0aa999e8b34f4680952fd19
Jam <jamisonline.he@gmail.com>
2023-11-08 23:52:00 +0800

    d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "44398fe2ef4ca3e0eb1058cb304db5bb",
-"index.html": "a82688068314a539e14f8b8fb7861a23",
-"/": "a82688068314a539e14f8b8fb7861a23",
-"main.dart.js": "492ebaf380999ba051bbf515c0abc41a",
+"index.html": "5397cb5bcf7688063e998f2623581d84",
+"/": "5397cb5bcf7688063e998f2623581d84",
+"main.dart.js": "cc7d3e0537ab9ae654865da329873d10",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
